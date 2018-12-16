5844f0edbd605899c2f5121151f6a7005f4088a2
Xavi <xavi.aznar@outlook.com>
2018-12-16 11:53:56 +0100

    Eliminada línea problemática.

--- vmconfig4ansible.sh
+++ vmconfig4ansible.sh
@@ -6,7 +6,6 @@ PYTHONPATH="$(which python)"
 
 if [ "$PYTHONPATH != "/usr/bin/python" ]; then
 	echo "Python interpreter\'s location is $PYTHONPATH"
-	echo "(should be /usr/bin/python)"
 	echo "Check it using \"which python\""
 	exit 2
 fi
