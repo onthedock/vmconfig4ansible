301c0b9260ce62886976906a78c670a4257d7e1e
Xavi <xavi.aznar@outlook.com>
2018-12-16 12:25:38 +0100

    Checking Python version

--- vmconfig4ansible.sh
+++ vmconfig4ansible.sh
@@ -1,5 +1,16 @@
 # Check Python version
-# python -V
+
+PYTHONVERSION_MAJOR="$(python -c \"import sys; print sys.version_info[0]\")"
+PYTHONVERSION_MINOR="$(python -c \"import sys; print sys.version_info[1]\")"
+
+if [ $PYTHONVERSION_MAJOR = 2 ]; then
+	if [ $PYTHONVERSION_MINOR <= 6 ]; then
+		echo "Minimum Python version should be 2.6 (it\'s $PITHONVERSION_MAJOR.$PYTHONVERSION_MINOR)"
+		exit 1
+	fi
+	sudo apt install python3
+fi
+
 # Check Python interpreter's location
 # which python --> /usr/bin/python
 PYTHONPATH="$(which python)"
