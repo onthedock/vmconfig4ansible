bdbe5b7a73d4fbb586374949e8f6659aa7af9b04
Xavi <xavi.aznar@outlook.com>
2018-12-16 11:39:36 +0100

    Corrige errores de sintaxis en el script.

--- vmconfig4ansible.sh
+++ vmconfig4ansible.sh
@@ -1,11 +1,11 @@
-# Check Python version
+Check Python version
 # python -V
 # Check Python interpreter's location
 # which python --> /usr/bin/python
 PYTHONPATH="$(which python)"
 
 if ["$PYTHONPATH != "/usr/bin/python" ]; then
-	echo "Python interpreter's location is $PYTHONPATH!"
+	echo "Python interpreter\'s location is $PYTHONPATH!"
 	echo "(should be /usr/bin/python)"
 	echo "Check it using \"which python\""
 	exit 2
