02e73510a1d6dde48443b9db3e6ff736714a46a6
Xavi <xavi.aznar@outlook.com>
2018-12-16 13:41:56 +0100

    Fix permissions

--- vmconfig4ansible.sh
+++ vmconfig4ansible.sh
@@ -21,5 +21,7 @@ sudo chmod 600 /home/$ANSIBLE_USER/.ssh/authorized_keys
 
 # Passwordless sudo
 echo "Configuring passwordless sudo..."
-sudo echo "$ANSIBLE_USER ALL=NOPASSWD: ALL" >> user-$ANSIBLE_USER
+echo "$ANSIBLE_USER ALL=NOPASSWD: ALL" >> user-$ANSIBLE_USER
 sudo mv user-$ANSIBLE_USER /etc/sudoers.d/
+# Fix permissions
+sudo chown root:root /etc/sudoers.d/user-$ANSIBLE_USER
